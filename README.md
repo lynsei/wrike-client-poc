1ae43830a221a016f67d0bc5dae0da2d5247cd83
Matthew Mayer <matthewkmayer@gmail.com>
2015-07-09 12:09:08 -0700

    Adds groundwork for refreshing oauth2 token.

--- README.md
+++ README.md
@@ -2,12 +2,20 @@
 
 Talking to the Wrike API.
 
-## Using envconfig to supply auth token
+## Using envconfig to supply tokens
+
 ```bash
 export WRIKECLIENTPOC_WRIKEBEARER="token_here"
+export WRIKECLIENTPOC_WRIKECLIENTID="id"
+export WRIKECLIENTPOC_WRIKECLIENTSECRET="secret"
 ```
-This will be added to the Authorization HTTP header with the format "bearer token_here".
+
+The bearer token will be added to the Authorization HTTP header with the format "bearer token_here".
+
+Other items will be used to refresh the token if needed.
 
 ## Wrike OAuth2 interaction
+
 https://developers.wrike.com/documentation/api/overview
+
 https://developers.wrike.com/faq/
--- main.go
+++ main.go
@@ -4,12 +4,15 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/kelseyhightower/envconfig"
 )
 
 type specification struct {
-	WrikeBearer string
+	WrikeBearer       string
+	WrikeClientID     string
+	WrikeClientSecret string
 }
 
 var s specification
@@ -39,9 +42,39 @@ func exampleRequest() {
 	log.Println(string(contents))
 }
 
+func refreshToken() {
+	log.Println("Refreshing token...")
+	client := &http.Client{}
+
+	body := makeRefreshBody()
+
+	req, err := http.NewRequest("POST", "https://www.wrike.com/oauth2/token", strings.NewReader(body))
+
+	resp, err := client.Do(req)
+	defer resp.Body.Close()
+
+	if err != nil {
+		log.Println("Client Error on refresh: ", err)
+		return
+	}
+	contents, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal("Error reading response body during refresh of token: ", err)
+	}
+	log.Println(string(contents))
+}
+
+func makeRefreshBody() string {
+	return "body"
+}
+
 func setup() {
 	err := envconfig.Process("wrikeclientpoc", &s)
 	if err != nil {
 		log.Fatal("boo: ", err.Error())
 	}
+
+	log.Println("bearer is " + s.WrikeBearer)
+	log.Println("client is " + s.WrikeClientID)
+	log.Println("secret is " + s.WrikeClientSecret)
 }
